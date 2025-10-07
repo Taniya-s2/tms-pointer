248d731c51d6418fefae55e6f58377b423d833e0
Taniya-s2 <taniyasamyani111@gmail.com>
2025-10-07 17:29:00 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+int i = 5, *j, *k;
+j = &i;
+j--;
+j -= 4;
+k = j - 5;
+printf("The value of pointer i is: %d\n", i);
+printf("The value of pointer j is: %d\n", *j);
+printf("The value of pointer k is: %d\n", k);
+return 0;
+}
